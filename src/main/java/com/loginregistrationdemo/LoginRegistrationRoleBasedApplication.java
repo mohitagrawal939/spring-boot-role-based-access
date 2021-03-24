8062abb1276a8d21ee7d84863d13e90c72ffb17f
Your Name <you@example.com>
2021-03-24 11:43:57 +0530

    Initial Commit

---
+++ src/main/java/com/loginregistrationdemo/LoginRegistrationRoleBasedApplication.java
@@ -0,0 +1,13 @@
+package com.loginregistrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationRoleBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationRoleBasedApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/loginregistrationdemo/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.loginregistrationdemo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
